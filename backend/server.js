490c14a74282c42f19a7ef1647aa66cef7d25968
menachemlev12@gmail.com <menachemlev12@gmail.com>
2021-11-02 00:51:48 +0200

    hf

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,6 @@ process.on('uncaughtException', (err) => {
 });
 console.log('runnig');
 const app = require('./app');
-const port = process.env.PORT || 8000;
+const PORT = process.env.PORT || 8000;
 
-app.listen(port, () => {});
+app.listen(PORT, () => {});
