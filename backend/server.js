20c704fde8367f1036a77af732af032bd65c6b9f
menachemlev12@gmail.com <menachemlev12@gmail.com>
2021-11-02 10:41:08 +0200

    h

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,16 @@
 /*const dotenv = require('dotenv');
+const path = require('path');
 dotenv.config({
-  path: './config.env',
+  path: path.resolve('config.env'),
 });*/
+
+console.log(process.env);
 process.on('uncaughtException', (err) => {
   console.log(err);
   process.exit(1);
 });
 console.log('runnig');
+const { defaultConfiguration } = require('./app');
 const app = require('./app');
 const PORT = process.env.PORT || 8000;
 
