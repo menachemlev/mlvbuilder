67d3066eae23460f950cefd78c35ed2d47d2615a
menachemlev12@gmail.com <menachemlev12@gmail.com>
2021-11-06 17:08:29 +0200

    background

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,12 @@
 import "./App.css";
 import React from "react";
+import { useEffect, useContext } from "React";
 import { Route, Switch } from "react-router-dom";
 import Home from "./pages/Home";
 import Header from "./components/Header/Header";
 import About from "./pages/About";
+
+import { useParams } from "react-router-dom";
 
 import Login from "./pages/Login";
 
@@ -12,8 +15,16 @@ import WebsitesList from "./pages/WebsitesList";
 import Account from "./pages/Account";
 import NotFound from "./pages/NotFound";
 import UserWebsite from "./pages/UserWebsite";
+import Auth from "./Auth/Auth";
 
 function App() {
+  const params = useParams();
+  const { login } = useContext(Auth);
+  useEffect(() => {
+    if (params.guest) {
+      login("guest@mlvbuilder.com", "12345678", "guest");
+    }
+  }, [params]);
   return (
     <div className="app">
       <Header />
--- frontend/src/Auth/Auth.js
+++ frontend/src/Auth/Auth.js
@@ -12,26 +12,19 @@ const Auth = createContext({
 });
 
 export const AuthProvider = (props) => {
-  const history = useHistory();
-  const params = useParams();
-
   const [loggedIn, setLoggedIn] = useState(false);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [name, setName] = useState("");
   const fetchProviderURL = "https://mlvbuilder.herokuapp.com";
   useEffect(() => {
-    let email = "guest@mlvbuilder.com",
-      password = "12345678";
-    if (!params.guest) {
-      if (typeof localStorage === undefined) return;
-      if (!localStorage.getItem("loginData")) return;
-      const localStorageData = JSON.parse(localStorage.getItem("loginData"));
-      const date = localStorageData.date;
-      email = localStorageData.email;
-      password = localStorage.password;
-      if (Date.now() > date + 90 * 24 * 60 * 60 * 1000) return logOut(); //30 days
-    }
+    if (typeof localStorage === undefined) return;
+    if (!localStorage.getItem("loginData")) return;
+    const { email, password, date, name } = JSON.parse(
+      localStorage.getItem("loginData")
+    );
+    if (Date.now() > date + 90 * 24 * 60 * 60 * 1000) return logOut(); //30 days
+
     fetch(`${fetchProviderURL}/users/auto-login`, {
       method: "POST",
       body: JSON.stringify({
