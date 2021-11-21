c77e3623f05d37f401c56b0b45358ae7daddb120
menachemlev12@gmail.com <menachemlev12@gmail.com>
2021-11-21 12:32:08 +0200

    ready for deploy

--- backend/app.js
+++ backend/app.js
@@ -62,7 +62,7 @@ app.options(
 );
 app.use(express.static(path.join(__dirname, './../frontend/build')));
 app.get('*', (req, res) =>
-  res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  res.sendFile(path.join(__dirname, './../frontend/build/index.html'))
 );
 //Route handler
 /////////////////////////////////////////
